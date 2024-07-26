0d0b787fbeae08e1044bc83e06080f254ee0c7f4
Nenad Vitorovic <nvitorovic@gmail.com>
2024-07-26 14:16:37 +0200

    adding token creation

--- packages/hardhat/deploy/01_deploy_pool.ts
+++ packages/hardhat/deploy/01_deploy_pool.ts
@@ -1,14 +1,15 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { tenderlyFund } from "../scripts/tenderly-fund";
+import { PoolCreator } from "../typechain-types";
 
-const deployPool: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployPoolCreator: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
   await tenderlyFund(deployer);
 
-  await deploy("PoolAndSwap", {
+  await deploy("PoolCreator", {
     from: deployer,
     // Contract constructor arguments
     args: [],
@@ -18,10 +19,16 @@ const deployPool: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
     autoMine: true,
     gasLimit: 8000000,
   });
+  const poolCreator = await hre.ethers.getContract<PoolCreator>("PoolCreator", deployer);
+  await poolCreator.deployPool(
+    "0x9af9daa3f7e1156e4a29e1063a2898175d0cd5f0",
+    "0x2efab7fe25bc03474f6a340e51a38744a284f8b2",
+    100,
+  );
 };
 
-export default deployPool;
+export default deployPoolCreator;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployPool.tags = ["DeployPool"];
+deployPoolCreator.tags = ["PoolCreator"];
--- packages/hardhat/deploy/02_deploy_erc-20-factory.ts
+++ packages/hardhat/deploy/02_deploy_erc-20-factory.ts
@@ -5,6 +5,22 @@ import { Contract, EventLog, parseEther, Wallet } from "ethers";
 import { ERC20TokenFactory } from "../typechain-types";
 import { tenderly } from "hardhat";
 
+async function createToken(hre: HardhatRuntimeEnvironment, deployer: string, tokenName: string, tokenSymbol: string) {
+  const tokenFactory = await hre.ethers.getContract<ERC20TokenFactory>("ERC20TokenFactory", deployer);
+  const tok = await tokenFactory.createToken(tokenName, tokenSymbol, parseEther("100000000000000000000000000000000"));
+  const receipt = await tok.wait();
+
+  const tstToken = (receipt!.logs.filter((log: any) => log.fragment?.name === "TokenCreated")[0] as EventLog).args[0];
+  console.log("Sending....");
+
+  const erc20Token = new Contract(
+    tstToken,
+    ["function transfer(address to, uint amount) returns (bool)"],
+    (await hre.ethers.getSigners())[0],
+  );
+  return erc20Token;
+}
+
 const deployErc20TokenFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
@@ -21,31 +37,30 @@ const deployErc20TokenFactory: DeployFunction = async function (hre: HardhatRunt
     autoMine: true,
     gasLimit: 8000000,
   });
-  const tokenFactory = await hre.ethers.getContract<ERC20TokenFactory>("ERC20TokenFactory", deployer);
-  const tok = await tokenFactory.createToken("Test", "TST", parseEther("100000000000000000000000000000000"));
-  const receipt = await tok.wait();
-
-  const tstToken = (receipt!.logs.filter((log: any) => log.fragment?.name === "TokenCreated")[0] as EventLog).args[0];
-  console.log("Sending....");
-
-  const erc20Token = new Contract(
-    tstToken,
-    ["function transfer(address to, uint amount) returns (bool)"],
-    (await hre.ethers.getSigners())[0],
-  );
+  const tokenFoo = await createToken(hre, deployer, "Foo Token", "FOO");
+  const tokenTst = await createToken(hre, deployer, "Bar Token", "BAR");
 
   console.log("Sending");
 
   await Promise.all([
-    erc20Token.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
-    erc20Token.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
-    erc20Token.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenTst.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenTst.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenTst.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenFoo.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenFoo.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
+    tokenFoo.transfer(Wallet.createRandom(hre.ethers.provider), parseEther("100000000")),
   ]);
 
-  await tenderly.verify({
-    name: "ERC20Token",
-    address: tstToken,
-  });
+  await tenderly.verify(
+    {
+      name: "ERC20Token",
+      address: await tokenTst.getAddress(),
+    },
+    {
+      name: "ERC20Token",
+      address: await tokenFoo.getAddress(),
+    },
+  );
 };
 
 export default deployErc20TokenFactory;
